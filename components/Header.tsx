8c474a1543193248f0075006bc7697b59fff3244
Mriganka Samanta <m.samanta.dev@gmail.com>
2024-03-17 15:52:52 -0400

    first commit

---
+++ components/Header.tsx
@@ -0,0 +1,18 @@
+function Header() {
+  return (
+    <header>
+      <div>
+        <img
+          src="https://rb.gy/ulxxee"
+          width={100}
+          height={100}
+          className="cursor-pointer object-contain"
+        />
+      </div>
+
+      <div></div>
+    </header>
+  );
+}
+
+export default Header;
